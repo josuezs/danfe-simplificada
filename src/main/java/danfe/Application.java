d9a999a6cd255838d787dc11136c59c1630ede75
Josue Schiochet <josue_schiochet@terceiros.sicredi.com.br>
2024-08-02 16:24:24 -0300

    Refactoring.

--- src/main/java/danfe/Application.java
+++ src/main/java/danfe/Application.java
@@ -26,16 +26,7 @@ public class Application {
 
             if (xmlFiles != null && xmlFiles.length > 0) {
                 log.info("Found {} XML files. Start reading..", xmlFiles.length);
-                var lstNfe = new ArrayList<TNfeProc>();
-                for (File xmlFile : xmlFiles) {
-                    try {
-                        var xmlContent = FileUtils.readFromFile(xmlFile);
-                        var nfeObject = (TNfeProc) XmlUtil.toObject(xmlContent, TNfeProc.class);
-                        lstNfe.add(nfeObject);
-                    } catch (Exception e) {
-                        log.error("Skipping XML {} with error {}", xmlFile, e.getMessage());
-                    }
-                }
+                var lstNfe = generateXmlIntoList(xmlFiles);
 
                 if (lstNfe.isEmpty()) {
                     log.info("No elegible XML found.");
@@ -49,6 +40,20 @@ public class Application {
         } catch (Exception e) {
             log.info("Error =/", e);
         }
+    }
+
+    private static ArrayList<TNfeProc> generateXmlIntoList(File[] xmlFiles) {
+        var lstNfe = new ArrayList<TNfeProc>();
+        for (File xmlFile : xmlFiles) {
+            try {
+                var xmlContent = FileUtils.readFromFile(xmlFile);
+                var nfeObject = (TNfeProc) XmlUtil.toObject(xmlContent, TNfeProc.class);
+                lstNfe.add(nfeObject);
+            } catch (Exception e) {
+                log.error("Skipping XML {} with error {}", xmlFile, e.getMessage());
+            }
+        }
+        return lstNfe;
     }
 
 }
